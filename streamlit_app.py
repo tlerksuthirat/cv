fd7032277e6850e56543eed6d40fd977b43b199b
Tassanee Lerksuthirat <tassanee.lerk@gmail.com>
2023-12-14 15:20:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title('Curriculum Vitae')
 st.header('Personal Details')
-st.markdown('**Name**      Tassanee Lerksuthirat')
+st.markdown('**Name:**&nbsp;&nbspTassanee Lerksuthirat')
