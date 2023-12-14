a1a355d3686e432d596165256487af1c7d8b3904
Tassanee Lerksuthirat <tassanee.lerk@gmail.com>
2023-12-14 14:58:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@ import streamlit as st
 st.title('Curriculum Vitae')
 st.header('Personal Details')
 name = st.text_input("Name")
-email = st.text_input("Email")
+email = st.text_input("Email")st.write('Name:', name)
+st.write('Email:', email)
+
